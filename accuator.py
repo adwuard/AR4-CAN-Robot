53e442b315beb076ea79315839459a81053f8baf
Hsuan Han Lai <p857211@gmail.com>
2024-01-05 18:46:53 +0800

    cfg

--- accuator.py
+++ accuator.py
@@ -6,7 +6,7 @@ ROBOT_CONFIG = AR4_CFG
 
 
 class Joint:
-    def __init__(self) -> None:
+    def __init__(self, joint_cfg) -> None:
     
         self.id = 1 
         self.driver = emm.EMMV5_MOROT()
@@ -27,9 +27,53 @@ class Joint:
     
     
     
+    def init_joint(self):
+        # setup zeroing
+        
+        pass
     
-    pass
+    def move_joint_schedule(self, angle):
+        # always use pos mode and abs on fixed direction
+        
+        # calc acceleration pulse count
+        
+        pass
+    
+    def move_joint_nonblocking(self, angle):
+        pass
+    
+    def check_arrived_flag(self):
+        pass
+    
+    def check_collision_flag(self):
+        pass
 
+
+class Joints:
+    def __init__(self, cfg) -> None:
+        self.homeing_sequence = cfg.homeing_sequence
+        self.joints = cfg.joints
+        
+        pass
+
+    def stop_all_joints(self):
+        pass
+    
+    def run_scheduled_task(self):
+        pass
+    
+    def check_all_arrived(self):
+        pass
+    
+    def homeing(self):
+        for s in self.homeing_sequence:
+            for j in s:
+                
+                pass
+        pass
+    
+        
+    
 
 
 
@@ -39,3 +83,12 @@ class Robot:
         
         pass
     
+    
+    
+""" 
+Joint Initialization:
+
+- 
+
+
+"""
--- ar4_configs.py
+++ ar4_configs.py
@@ -1,8 +1,8 @@
 class AR4_CONFIG:
     joint_1 = {
         'name': 'Joint-1-Shouder-Swing',
-        'id': 0x01,
-        'enabled': True,
+        'motor_addr': 0x01,
+        'enable': True,
         'motor_direction': 0, # 1 or -1
         'microstep': 128,
         'steps_per_rev': 200,
@@ -17,13 +17,13 @@ class AR4_CONFIG:
         'home_method': 'endswitch',
         'max_angle': 170,
         'min_angle': -170,
-
+        'calibreation_angle_offset': 0,
     }
 
     joint_2 = {
         'name': 'Joint-2-Shouder-Rotate',
-        'id': 0x02,
-        'enabled': True,
+        'motor_addr': 0x02,
+        'enable': True,
         'motor_direction': 1, # 1 or -1
         'microstep': 128,
         'steps_per_rev': 200,
@@ -38,12 +38,13 @@ class AR4_CONFIG:
         'home_method': 'endswitch',
         'max_angle': 90,
         'min_angle': -42,
+        'calibreation_angle_offset': 0,
     }
 
     joint_3 = {
         'name': 'Joint-3-Elbow-Swing',
-        'id': 0x03,
-        'enabled': True,
+        'motor_addr': 0x03,
+        'enable': True,
         'microstep': 128,
         'steps_per_rev': 200,
         'gear_ratio': 1,
@@ -57,13 +58,13 @@ class AR4_CONFIG:
         'home_method': 'endswitch',
         'max_angle': 52,
         'min_angle': -89,
-
+        'calibreation_angle_offset': 0,
     }
 
     joint_4 = {
         'name': 'Joint-4-Elbow-Rotate',
-        'id': 0x04,
-        'enabled': True,
+        'motor_addr': 0x04,
+        'enable': True,
         'motor_direction': 1, # 1 or -1
         'microstep': 128,
         'steps_per_rev': 200,
@@ -78,12 +79,13 @@ class AR4_CONFIG:
         'home_method': 'endswitch',
         'max_angle': 165,
         'min_angle': -165,
+        'calibreation_angle_offset': 0,
     }
 
     joint_5 = {
         'name': 'Joint-5-Wrist-Swing',
-        'id': 0x05,
-        'enabled': True,
+        'motor_addr': 0x05,
+        'enable': True,
         'motor_direction': 1, # 1 or -1
         'microstep': 128,
         'steps_per_rev': 200,
@@ -98,13 +100,14 @@ class AR4_CONFIG:
         'home_method': 'endswitch',
         'max_angle': 105,
         'min_angle': -105,
+        'calibreation_angle_offset': 0,
 
     }
 
     joint_6 = {
         'name': 'Joint-6-Wrist-Rotate',
-        'id': 0x06,
-        'enabled': True,
+        'motor_addr': 0x06,
+        'enable': True,
         'motor_direction': 1, # 1 or -1
         'microstep': 128,
         'steps_per_rev': 200,
@@ -119,12 +122,13 @@ class AR4_CONFIG:
         'home_method': 'endswitch',
         'max_angle': 155,
         'min_angle': -155,
+        'calibreation_angle_offset': 0,
     }
 
     joint_7 = {
         'name': 'Joint-7-Gripper',
-        'id': 0x07,
-        'enabled': True,
+        'motor_addr': 0x07,
+        'enable': True,
         'motor_direction': 1, # 1 or -1
         'microstep': 128,
         'steps_per_rev': 200,
@@ -139,12 +143,13 @@ class AR4_CONFIG:
         'home_method': 'endswitch',
         'max_angle': 3333,
         'min_angle': 0,
+        'calibreation_angle_offset': 0,
     }
 
     joint_8 = {
         'name': 'Joint-8-Base-Slider',
-        'id': 0x08,
-        'enabled': False,
+        'motor_addr': 0x08,
+        'enable': False,
         'motor_direction': 1, # 1 or -1
         'microstep': 128,
         'steps_per_rev': 200,
@@ -159,22 +164,18 @@ class AR4_CONFIG:
         'home_method': 'endswitch',
         'max_angle': 3333,
         'min_angle': 0,
+        'calibreation_angle_offset': 0,
     }
 
-
-    robot_configs = {
-        'joints': [joint_1, joint_2, joint_3, joint_4, joint_5, joint_6, joint_7, joint_8],
-        'homeing_sequence': [[1,2,3,4,5],[6,7,8]],
-        'dh_params': [
+    joints  = [joint_1, joint_2, joint_3, joint_4, joint_5, joint_6, joint_7, joint_8]
+    homeing_sequence = [[1,2,3,4,5],[6,7,8]]
+    dh_params = [
             [    0,    -90,  169.77,   64.2  ],
             [  -90,      0,       0,    305  ],
             [  180,     90,       0, -.0001  ],
             [    0,    -90,  222.63,      0  ],
             [    0,     90,       0,      0  ],
             [    0,      0,   36.25,      0  ]
-        ],
-        
-        
-    }
+        ]
 
 AR4_CFG = AR4_CONFIG()
